64288d9e79a0ff56977f35634683cec85da1b625
korneev.r <rftgyhuj123987>
2021-07-30 17:49:57 +0300

    Change Admin Panel Interface

--- src/invest_web/settings.py
+++ src/invest_web/settings.py
@@ -35,6 +35,9 @@ ALLOWED_HOSTS = ['*']
 # Application definition
 
 INSTALLED_APPS = [
+    'admin_interface',
+    'colorfield',
+
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
